45c2c1ee5373befe45578edd525a0974ca5d551d
henrik <>
2021-09-11 21:49:04 +0200

    Remove comment

--- qemu.sh
+++ qemu.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 qemu-system-x86_64 -bios /usr/share/qemu/OVMF.fd -drive format=raw,file=disk.img -enable-kvm \
 -netdev tap,id=mynet0,ifname=tap0 -device e1000,netdev=mynet0
-
-#qemu-system-x86_64 -kernel /home/henrik/misc/linux-5.14.2/arch/x86/boot/bzImage -drive format=raw,file=disk.img -enable-kvm \
-#-netdev tap,id=mynet0,ifname=tap0 -device e1000,netdev=mynet0
